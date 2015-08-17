6a090705eb2dfa35052cad7985958435b36e1e22
Jeff Steward <steward.jeff@gmail.com>
2015-08-17 17:50:19 -0400

    added error handling when the API returns a bad object

--- web.js
+++ web.js
@@ -40,86 +40,88 @@ http.createServer(function(request, response) {
 		var output = syncrequest('GET', 'http://api.harvardartmuseums.org/object/' + objectid + "?apikey=" + apikey);
 		var object = JSON.parse(output.getBody());		
 
-		//Get the image of the object
-		var imageParameters = "width=840&height=500";
-		var image, imageDims;
-		if (object.imagepermissionlevel > 0) {
-			imageParameters = "width=256&height=256";
-		}
-		if (object.images[0]) {
-			image = syncrequest('GET', "http://ids.lib.harvard.edu/ids/view/" + object.images[0].idsid + "?" + imageParameters);
-			imageDims = sizeOf(image.getBody());
-		}
+		if (!object.error) {
+			//Get the image of the object
+			var imageParameters = "width=840&height=500";
+			var image, imageDims;
+			if (object.imagepermissionlevel > 0) {
+				imageParameters = "width=256&height=256";
+			}
+			if (object.images[0]) {
+				image = syncrequest('GET', "http://ids.lib.harvard.edu/ids/view/" + object.images[0].idsid + "?" + imageParameters);
+				imageDims = sizeOf(image.getBody());
+			}
 
-		//Build the object data in to a description text block
-		var description = "";
-		if (object.titles) {
-			object.titles.forEach(function(title) {
-				description += title.titletype + ": " + title.title + "\n";
-			});
-		}
-		if (object.people) {
-			object.people.forEach(function(person) {
-				description += person.role + ": " + person.displayname + "\n";
-			});
-		}
-		if (object.classification) {
-			description += "Classification: " + object.classification + "\n";
-		}
-		if (object.worktypes) {
-			var workTypes = "";
-			for (var i = 0; i < object.worktypes.length; i++) {
-				workTypes += object.worktypes[i].worktype;
-				if (i < object.worktypes.length-1) workTypes += ", ";
-			};
-			description += "Work Type: " + workTypes + "\n";
-		}
-		if (object.culture) {
-			description += "Culture: " + object.culture + "\n";
-		}
-		if (object.places) {
-			object.places.forEach(function(place) {
-				description += place.type + ": " + place.displayname + "\n";
-			});
-		}
-		if (object.dated) {
-			description += "Date: " + object.dated + "\n";
-		}
-		if (object.century) {
-			description += "Century: " + object.century + "\n";
-		}
-		if (object.period) {
-			description += "Period: " + object.period + "\n";
-		}
-		if (object.medium) {
-			description += "Medium: " + object.medium + "\n";
-		}
-		if (object.technique) {
-			description += "Technique: " + object.technique + "\n";
-		}
-		if (object.dimensions) {
-			description += "Dimensions: " + object.dimensions.replace("\r\n", "; ") + "\n";
-		}
-		description += "Credit line: " + object.creditline + "\n";
-		description += "Object number: " + object.objectnumber + "\n";
-		if (object.copyright) {
-			description += "Copyright: " + object.copyright + "\n";
-		}
-		description += "Division: " + object.division + "\n";
-		description += "URL: " + object.url + "\n";
+			//Build the object data in to a description text block
+			var description = "";
+			if (object.titles) {
+				object.titles.forEach(function(title) {
+					description += title.titletype + ": " + title.title + "\n";
+				});
+			}
+			if (object.people) {
+				object.people.forEach(function(person) {
+					description += person.role + ": " + person.displayname + "\n";
+				});
+			}
+			if (object.classification) {
+				description += "Classification: " + object.classification + "\n";
+			}
+			if (object.worktypes) {
+				var workTypes = "";
+				for (var i = 0; i < object.worktypes.length; i++) {
+					workTypes += object.worktypes[i].worktype;
+					if (i < object.worktypes.length-1) workTypes += ", ";
+				};
+				description += "Work Type: " + workTypes + "\n";
+			}
+			if (object.culture) {
+				description += "Culture: " + object.culture + "\n";
+			}
+			if (object.places) {
+				object.places.forEach(function(place) {
+					description += place.type + ": " + place.displayname + "\n";
+				});
+			}
+			if (object.dated) {
+				description += "Date: " + object.dated + "\n";
+			}
+			if (object.century) {
+				description += "Century: " + object.century + "\n";
+			}
+			if (object.period) {
+				description += "Period: " + object.period + "\n";
+			}
+			if (object.medium) {
+				description += "Medium: " + object.medium + "\n";
+			}
+			if (object.technique) {
+				description += "Technique: " + object.technique + "\n";
+			}
+			if (object.dimensions) {
+				description += "Dimensions: " + object.dimensions.replace("\r\n", "; ") + "\n";
+			}
+			description += "Credit line: " + object.creditline + "\n";
+			description += "Object number: " + object.objectnumber + "\n";
+			if (object.copyright) {
+				description += "Copyright: " + object.copyright + "\n";
+			}
+			description += "Division: " + object.division + "\n";
+			description += "URL: " + object.url + "\n";
 
-		//Put the image and description in the slide
-		var textVerticalOffset = 40;
+			//Put the image and description in the slide
+			var textVerticalOffset = 40;
 
-		slide = pptx.makeNewSlide();
-		slide.back = 'ffffff';
-		slide.color = '000000';
-		slide.name = object.objectnumber;
-		if (image) {
-			slide.addImage(image.getBody(), {y: 20, x: 'c', cx: imageDims.width, cy: imageDims.height});
-			textVerticalOffset += imageDims.height
+			slide = pptx.makeNewSlide();
+			slide.back = 'ffffff';
+			slide.color = '000000';
+			slide.name = object.objectnumber;
+			if (image) {
+				slide.addImage(image.getBody(), {y: 20, x: 'c', cx: imageDims.width, cy: imageDims.height});
+				textVerticalOffset += imageDims.height
+			}
+			slide.addText(description, {y: textVerticalOffset, cx: '100%', font_size: 10, font_face: 'Arial', bold: false, color: '000000', align: 'left'});
 		}
-		slide.addText(description, {y: textVerticalOffset, cx: '100%', font_size: 10, font_face: 'Arial', bold: false, color: '000000', align: 'left'});
 	});
 
 	pptx.generate(response);
