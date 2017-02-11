254824804ea89f7622e02ea691f79401ce09e302
Callahan, Ian <ian_callahan@harvard.edu>
2017-02-11 01:46:11 +0000

    Use stringify.

--- index.js
+++ index.js
@@ -151,6 +151,7 @@ exports.handler = (event, context, callback) => {
     	console.log("s3");
     	var JSONpayload = new Array();
     	JSONpayload['pptx'] = filename;
+    	data = JSON.stringify(JSONpayload);
     	s3.upload(param, function(err, data) {
         	if (err) console.log(err, err.stack); // an error occurred
         	else console.log(data);           // successful response
