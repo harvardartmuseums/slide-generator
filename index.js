87bfcbafab4ea2dac2ba5800a4732f6b2ce209fc
Callahan, Ian <ian_callahan@harvard.edu>
2017-02-13 17:54:00 +0000

    Wrong concat syntax

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ exports.handler = (event, context, callback) => {
     	});
     console.log('done');
 
-    callback(null, {"location": "https://s3.amazonaws.com/slides.harvardartmuseums.org/" . filename});
+    callback(null, {"location": "https://s3.amazonaws.com/slides.harvardartmuseums.org/" + filename});
 		});
 	}
  
